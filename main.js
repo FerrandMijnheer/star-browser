a4227b56463b545d7103785530be19942611cd8c
Superrtiger <mijnheerf@gmail.com>
2024-06-26 12:06:43 +0200

    Changed reload + add new windows button + update styles + add loading icon + add dynamic window text

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 const path = require('node:path')
 const iconPath = path.join(__dirname, "images", "logos", "256.png")
 
 
 app.whenReady().then(() => {
+  createWindow();
+});
+
+function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 1080,
     icon: iconPath,
     height: 720,
     webPreferences: {
-      webviewTag: true
+      webviewTag: true,
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
   mainWindow.loadFile('index.html')
-});
+}
+
+ipcMain.on("open-new-window", () => {
+  createWindow();
+})
 
 app.on('window-all-closed', () => {
   app.quit()
 });
-
