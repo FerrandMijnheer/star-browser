29e32e313323877f09e13b3c7a71837229395315
Superrtiger <mijnheerf@gmail.com>
2024-06-26 19:16:05 +0200

    Clean up code in mutiple files

--- main.js
+++ main.js
@@ -1,30 +1,29 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
-const path = require('node:path')
-const iconPath = path.join(__dirname, "images", "logos", "256.png")
-
+const { app, BrowserWindow, ipcMain } = require("electron/main");
+const path = require("node:path");
+const iconPath = path.join(__dirname, "images", "logos", "256.png");
 
 app.whenReady().then(() => {
   createWindow();
 });
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1080,
     icon: iconPath,
     height: 720,
     webPreferences: {
       webviewTag: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile("index.html");
 }
 
 ipcMain.on("open-new-window", () => {
   createWindow();
-})
+});
 
-app.on('window-all-closed', () => {
-  app.quit()
+app.on("window-all-closed", () => {
+  app.quit();
 });
